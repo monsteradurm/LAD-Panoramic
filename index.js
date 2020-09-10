bcfd698dcf80f5f929add29c16c738861cad5d6d
monsteradurm <acranch@liquidanimation.com>
2020-09-10 13:01:21 +1000

    initial

---
+++ index.js
@@ -0,0 +1,8 @@
+import {PanoViewer} from "@egjs/view360";
+
+const panoViewer = new PanoViewer(
+    document.getElementById("myPanoViewer"),
+    {
+      image: "/images/MF_Equilateral.jpg"
+    }
+  );
